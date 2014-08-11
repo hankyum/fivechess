d8dac7adff64b4e80066c11fde9615a8443458a1
hankyum <lakeguo@gmail.com>
2014-08-11 10:58:28 +0800

    init

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,26 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+@RestController
+class Controller {
+	@RequestMapping(value="/")
+	public String index() {
+		return "Hello";
+	}
+}
